a5950347fcf94fc6d93f0de85095f9ccd3d85866
Saran Kongdam <68011813@kmitl.ac.th>
2026-03-25 10:16:49 +0700

    p01.2

--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -1,5 +1,4 @@
-import { Injectable } from "@nestjs/common";
-import { ProductModule } from "./product.module";
+import { Injectable } from '@nestjs/common';
 import { Product } from "./product.interface";
 import * as path from 'path';
 import * as fs from 'fs';
@@ -8,10 +7,9 @@ import * as fs from 'fs';
 export class ProductService {
 private readonly dbPath = 'data/products.json';
 
-    findAll(): Product<T> {
+    findAll() {
         const filePath = path.resolve(process.cwd(),'data/products.json');
         const fileContent = fs.readFileSync(filePath, 'utf-8');
-        const users: Product<T> = JSON.parse(fileContent);
         return JSON.parse(fileContent);
 }
  
